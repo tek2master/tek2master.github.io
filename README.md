7570ffafa9eddfa47070bbb1e8d668c70b4585c0
Thomas Kenney Jr <tek2master@msn.com>
2017-08-23 19:56:56 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# tek2master.github.io
+# EASY / MEDIUM
+
+1. Change the heading from "Random Student Picker" to "Your Name's
+   random student picker.
+
+2. Change the page's font.
+
+3. Change the logo to an image of your choice.
+
+4. Get rid of those dots!
+
+
+# HARD
+
+1. Allow each student to be chosen only once. (HINT: you will have to
+   modify the `students` array.
+
+
+# HELLMODE
+
+1. Make it so that striking out a student's name in the attendance
+   list removes that student from the random selection.
+
+2. Create a new button labeled PAIRS that will create and display
+   random pairings of students.
---
+++ rand.js
@@ -0,0 +1,75 @@
+var pickButton;
+var header;
+var body;
+var choice;
+var aList;
+
+var students = [
+  "Kevin",
+  "Jonathan",
+  "Angel",
+  "Eric",
+  "Janette",
+  "Desmond",
+  "Ashiya",
+  "Monica",
+  "Susana",
+  "Deborah",
+  "Thomas",
+  "Julissa",
+  "Kyla",
+  "John",
+  "Shonica",
+  "Krystal"
+];
+
+var colors = [
+  'hotpink',
+  'orange',
+  'fuchsia', 0,
+  'lightblue', 0,
+  'black'
+];
+var cur = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('hello rand.js');
+
+  body = document.getElementById('body');
+  header = document.getElementById('header');
+  pickButton = document.getElementById('pickButton');
+  choice = document.getElementById('choice');
+  aList = document.getElementById('aList');
+
+  pickButton.addEventListener('click', pickOnClick );
+  aList.addEventListener('click', aListOnClick );
+
+  // build the attendance list
+  for (let i=0; i<students.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = students[i];
+    aList.append(li);
+  }
+});
+
+var pickOnClick = function (event) {
+  choice.innerHTML = '&nbsp;'
+  var rand = students[Math.floor(Math.random() * students.length)];
+  var x = window.setInterval(() => {
+    if (colors[cur] === undefined) {
+      window.clearInterval(x);
+      cur = 0;
+      choice.innerText = rand;
+      return;
+    }
+    if (colors[cur]) header.style.color = colors[cur];
+    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
+    cur += 1;
+  }, 200);
+}
+
+var aListOnClick = function (event) {
+  if (event.target.tagName === 'LI') {
+    event.target.style.textDecoration = 'line-through';
+  }
+}
