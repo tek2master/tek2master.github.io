e66feca4f2fb205d958a078a8e2a22cc07359471
Thomas Kenney Jr <tek2master@msn.com>
2017-08-25 10:00:51 -0400

    Update rand.js

--- rand.js
+++ rand.js
@@ -46,9 +46,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // build the attendance list
   for (let i=0; i<students.length; i++) {
-    let li = document.createElement('li');
-    li.innerText = students[i];
-    aList.append(li);
+    let ul = document.createElement('ul');
+    ul.innerText = students[i];
+    aList.append(ul);
   }
 });
 
